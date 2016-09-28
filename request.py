ae7b1f4e7a46f642bcfaadb40a497502a03334d0
aceLeVillegas <sarahvillegas95@gmail.com>
2016-09-27 19:08:23 -0700

    Removed all keys from the files

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-keys ={"token":"c7e21aa496fe24dfc7fc0a9cd21627ef", "github": "https://github.com/aceLeVillegas/Code2040Challenge"}
+keys ={"token":"", "github": "https://github.com/aceLeVillegas/Code2040Challenge"}
 
 response = requests.post("http://challenge.code2040.org/api/register", keys )
 
